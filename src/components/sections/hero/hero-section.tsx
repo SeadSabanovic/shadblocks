3b741a9ea08217ebe7c9b40128bfbdaa5dc152cd
Sead Sabanovic <sead.sabanovic97@gmail.com>
2025-08-16 18:01:34 +0200

    Remove unused peep SVG images and update hero section
    Deleted peep1.svg, peep2.svg, and peep3.svg from the assets/images/peeps directory. Updated the hero-section.tsx component to reflect the removal of these assets, likely as part of a cleanup or redesign.

--- src/components/sections/hero/hero-section.tsx
+++ src/components/sections/hero/hero-section.tsx
@@ -7,24 +7,6 @@ import next from "@/assets/icons/next.svg";
 import gsap from "@/assets/icons/gsap.svg";
 import shadcn from "@/assets/icons/shadcn.svg";
 import ts from "@/assets/icons/ts.svg";
-import peep1 from "@/assets/images/peeps/peep1.svg";
-import peep2 from "@/assets/images/peeps/peep2.svg";
-import peep3 from "@/assets/images/peeps/peep3.svg";
-
-const avatarImages = [
-  {
-    src: peep1,
-    alt: "User avatar 1",
-  },
-  {
-    src: peep2,
-    alt: "User avatar 2",
-  },
-  {
-    src: peep3,
-    alt: "User avatar 3",
-  },
-];
 
 const techIcons = [
   {
@@ -51,10 +33,16 @@ const techIcons = [
 
 export default function HeroSection() {
   return (
-    <section className="mx-4 mb-4 flex h-[calc(100svh-100px)] max-h-[1240px] flex-col overflow-hidden rounded-2xl border bg-gray-50 py-16">
-      <Container className="z-10 grid h-fit grid-cols-12 grid-rows-5 flex-col justify-center gap-4">
-        <div className="col-span-8 flex flex-col gap-4">
-          <H1 className="text-4xl lg:text-6xl">Shadcn marketing Blocks</H1>
+    <section className="mx-4 mb-4 flex h-[calc(100svh-100px)] max-h-[1240px] flex-col overflow-hidden rounded-2xl border py-16">
+      <Container className="relative z-10 flex h-fit flex-1 flex-col items-center justify-center gap-4">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <H1 className="text-4xl lg:text-6xl">
+            Shad
+            <span className="bg-gradient-to-b from-[#000000] to-[#666666] bg-clip-text text-transparent">
+              cn
+            </span>{" "}
+            marketing Blocks
+          </H1>
           <H3 className="uppercase">Accessible. SEO friendly. Free forever.</H3>
           <Badge
             variant="outline"
@@ -76,6 +64,10 @@ export default function HeroSection() {
             ))}
           </Badge>
         </div>
+
+        <div className="bg-border h-40 w-[1px]"></div>
+        <div className="absolute top-1/2 left-1/2 -z-10 h-96 w-96 -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-[100px] bg-gradient-to-r from-pink-100/20 via-pink-50/20 to-pink-600/20 blur-3xl [animation-duration:2s]" />
+        <div className="absolute top-1/2 left-1/2 -z-20 h-80 w-80 -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-[80px] bg-gradient-to-r from-gray-100/30 via-gray-200/30 to-gray-50/30 blur-2xl [animation-duration:2s]" />
       </Container>
     </section>
   );
